71b643b9485e16cf36a63de5d693aaaa7fb718da
kakdidi <kakdidi@gmail.com>
2021-03-10 09:45:22 +0000

    adding conditions

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 print("Welcome to my first game!")
-name = input("What is your name?")
-age = input("What is your age?")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 
 print("Hello", name, "you are", age, "years old")
+
+if age >= 18:
+    print("You are old enough to play!")
+    wants_to_play = input("Do you want to play? ")
+    if wants_to_play == "yes":
+        print("Let's Play!!")
+else:
+    print("You aren't enough to play")
